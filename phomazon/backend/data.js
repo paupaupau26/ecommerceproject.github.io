76dce9c1780464561ac05c598f7604f3a931fadf
unknown <josiasmichael@gmail.com>
2022-11-11 22:40:59 +0800

    added backend nodemon

---
+++ phomazon/backend/data.js
@@ -0,0 +1,50 @@
+const data = {
+  products: [
+    {
+      name: 'Samsung S22 Ultra',
+      slug: 'samsung-s22-ultra',
+      category: 'Phones',
+      image: '/images/samsungs22ultra.jpg',
+      price: 990,
+      countInStock: 10,
+      brand: 'Samsung',
+      numReviews: 10,
+      description: '2022 Samsung Flagship',
+    },
+    {
+      name: 'iPhone 14 Pro Max',
+      slug: 'iphone-14-pro-max',
+      category: 'Phones',
+      image: '/images/iphone14promax.jpg',
+      price: 1190,
+      countInStock: 15,
+      brand: 'Apple',
+      numReviews: 10,
+      description: '2022 Apple Flagship',
+    },
+    {
+      name: 'Google Pixel 5 Pro',
+      slug: 'google-pixel-5pro',
+      category: 'Phones',
+      image: '/images/googlepixel5pro.jpg',
+      price: 800,
+      countInStock: 10,
+      brand: 'Google',
+      numReviews: 10,
+      description: '2022 Google Flagship',
+    },
+    {
+      name: 'Motorola Razr 5G 2020',
+      slug: 'motorola-razr-5g-2020',
+      category: 'Phones',
+      image: '/images/motorazr2020.jpg',
+      price: 790,
+      countInStock: 20,
+      brand: 'Motorola',
+      numReviews: 30,
+      description: '2020 Samsung Flagship',
+    },
+  ],
+};
+
+export default data;
