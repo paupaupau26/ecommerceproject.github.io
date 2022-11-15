ecd6ef478771e04f69ae96fcd1dd658e96a30755
paupaupau26 <113437105+paupaupau26@users.noreply.github.com>
2022-11-15 19:36:08 +0800

    added function in cartscreen and signin form

--- phomazon/backend/server.js
+++ phomazon/backend/server.js
@@ -1,6 +1,19 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI).then(() =>{
+  console.log('connected to db')
+}).catch(err =>{
+  console.log(err.message);
+});
+
+
 const app = express();
+
 
 app.get('/api/products', (req, res) => {
   res.send(data.products);
@@ -27,4 +40,5 @@ app.get('/api/products/:id', (req, res) => {
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
+  
 });
